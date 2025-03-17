86198064d300b23d19c9781ceb33e877c58c3f26
tridi003 <ghoshtridibesh@gmail.com>
2025-03-17 18:46:56 +0530

    setup complete

--- notebooks/# THis is the file to setup a new projec.py
+++ notebooks/# THis is the file to setup a new projec.py
@@ -1,1 +1,14 @@
-# THis is the file to setup a new project in Vs code with git hub integration
+## THis is the file to setup a new project in Vs code with git hub integration
+
+# Link to the setup
+# https://datalumina.clickup.com/docs/9015213037/d/h/8cnjezd-17675/ddd52c673443975
+# Video to the setup
+# https://youtu.be/mpk4Q5feWaw
+
+
+# test code
+
+import pandas as pd
+import numpy as np
+
+#! pip install pandas
